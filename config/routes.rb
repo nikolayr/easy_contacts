50942de24854f23f5f5f971850a6d3f4a82ca3aa
Nikolay Ruban <nikolayr@gmail.com>
2015-10-19 23:48:11 +0300

    must rewrite and take activity from journal

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@
 #post "easy_contacts/new(.:format)" => "easy_contacts#create"
 #delete "easy_contacts/:id(.:format)" => "easy_contacts#destroy"
 
+get "easy_contacts/:id(.format)" =>"easy_contacts#show"
+
 get "projects/:project_id/easy_contacts(.:format)" => "easy_contacts#index"
 get "projects/:project_id/easy_contacts/new(.:format)" => "easy_contacts#new"
 get "projects/:project_id/easy_contacts/:id(.:format)" => "easy_contacts#show"
