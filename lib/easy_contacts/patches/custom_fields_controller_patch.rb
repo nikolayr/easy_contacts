3171f72e6293991c74faaa6b80678f57958b970e
Nikolay Ruban <nikolayr@gmail.com>
2015-10-24 01:28:45 +0300

    avg branch

--- lib/easy_contacts/patches/custom_fields_controller_patch.rb
+++ lib/easy_contacts/patches/custom_fields_controller_patch.rb
@@ -6,40 +6,39 @@ module EasyContacts
         base.send(:include, InstanceMethods)
 
         base.class_eval do
-#          alias_method_chain :render_custom_fields_tabs, :add_custom_client_field
-#          alias_method_chain :custom_field_type_options, :custom_client_field
+          alias_method_chain :initialize, :add_custom_contact_tab
+          alias_method_chain :custom_field_type_options, :custom_client_field
         end
       end
 
       module ClassMethods
-      end
+      end #ClassMethods
 
       module InstanceMethods
 
-        # def initialize(*args)
-        #   puts "pops"
-        #   easy_contact_field_tab
-        # end
+        def initialize_with_add_custom_contact_tab(*args)
+          puts "pre init CustomFieldsHelper::CUSTOM_FIELDS_TABS.size:#{CustomFieldsHelper::CUSTOM_FIELDS_TABS.size}"
+          easy_contact_field_tab
+        end
+
+        def redef_without_warning(const, value)
+          self.class.send(:remove_const, const) if self.class.const_defined?(const)
+          self.class.const_set(const, value)
+        end
 
         def easy_contact_field_tab
           @@ec_tab_added ||= false
           unless @@ec_tab_added
-            # CustomFieldsHelper::CUSTOM_FIELDS_TABS << {:name => 'EasyContactsCustomField',
-            #                                            :partial => 'custom_fields/index',
-            #                                            :label => :label_easy_contacts_plural}
+            custom_fields = CustomFieldsHelper::CUSTOM_FIELDS_TABS << {:name => 'EasyContactsCustomField',
+                                                                       :partial => 'custom_fields/index',
+                                                                       :label => :label_easy_contacts_plural}
+            redef_without_warning 'CUSTOM_FIELDS_TABS', custom_fields
             @@ec_tab_added = true
           end
         end
 
-        def render_custom_fields_tabs_with_add_custom_client_field(*args)
-          easy_contact_field_tab
-          tabs = CUSTOM_FIELDS_TABS.select {|h| types.include?(h[:name]) }
-          render_tabs tabs
-
-        end
-
-        def custom_field_type_options_with_custom_client_field(*args)
-          easy_contact_field_tab
+        def custom_field_type_options_with_custom_client_field
+          puts "CustomFieldsHelper::CUSTOM_FIELDS_TABS.size:#{CustomFieldsHelper::CUSTOM_FIELDS_TABS.size}"
         end
 
       end #InstanceMethods
