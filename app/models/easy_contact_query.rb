585e232eb79708b1ade60d48786a02aeb16348d0
Nikolay Ruban <nikolayr@gmail.com>
2015-11-04 15:08:22 +0300

    filter works

--- app/models/easy_contact_query.rb
+++ app/models/easy_contact_query.rb
@@ -7,15 +7,15 @@
         QueryColumn.new(:first_name, :sortable => "#{EasyContact.table_name}.first_name"),
         QueryColumn.new(:last_name, :sortable => "#{EasyContact.table_name}.last_name"),
         QueryColumn.new(:date_created, :sortable => "#{EasyContact.table_name}.date_created"),
-        QueryColumn.new(:author_id, :sortable => "#{EasyContact.table_name}.author_id"),
-        QueryColumn.new(:project_id, :sortable => "#{EasyContact.table_name}.project_id")
+        QueryColumn.new(:author_id, :sortable => "#{EasyContact.table_name}.author_id")
+#        QueryColumn.new(:project_id, :sortable => "#{EasyContact.table_name}.project_id")
     ]
 
     scope :visible, -> { where(visible: true) }
 
     def initialize(attributes=nil, *args)
       super attributes
-      self.filters ||= { 'project_id' => {:operator => "o", :values => [""]} }
+      self.filters ||= { 'project_id' => {:operator => "=", :values => ["1"]} }
     end
 
     # Returns true if the query is visible to +user+ or the current user.
@@ -61,8 +61,8 @@
       add_available_filter "last_name", :type => :text
       add_available_filter "date_created", :type => :date
 
-     custom_fields = EasyContactCustomField.where(is_for_all: false)
-     add_custom_fields_filters(custom_fields)
+     #custom_fields = EasyContactCustomField.where(is_for_all: false)
+     #add_custom_fields_filters(custom_fields)
 
      #add_associations_custom_fields_filters :project, :author, :assigned_to, :fixed_version
 
@@ -102,9 +102,6 @@
           offset(options[:offset])
 
       scope = scope.preload(:custom_values)
-      if has_column?(:author)
-        scope = scope.preload(:author)
-      end
 
       scope.all
     rescue ::ActiveRecord::StatementInvalid => e
@@ -137,7 +134,34 @@
 
    def statement
      # TODO return query statements to filter contacts
-     ''
+     filters_clauses = []
+     filters.each_key do |field|
+       next if field == "subproject_id"
+       v = values_for(field).clone
+       next unless v and !v.empty?
+       operator = operator_for(field)
+
+       if field =~ /cf_(\d+)$/
+         # custom field
+         filters_clauses << sql_for_custom_field(field, operator, v, $1)
+       elsif respond_to?("sql_for_#{field}_field")
+         # specific statement
+         filters_clauses << send("sql_for_#{field}_field", field, operator, v)
+       else
+         # regular field
+         filters_clauses << '(' + sql_for_field(field, operator, v, queried_table_name, field) + ')'
+       end
+     end if filters and valid?
+
+     if (c = group_by_column) && c.is_a?(QueryCustomFieldColumn)
+       # Excludes results for which the grouped custom field is not visible
+       filters_clauses << c.custom_field.visibility_by_project_condition
+     end
+
+     filters_clauses << project_statement
+     filters_clauses.reject!(&:blank?)
+
+     filters_clauses.any? ? filters_clauses.join(' AND ') : nil
    end
 
 
